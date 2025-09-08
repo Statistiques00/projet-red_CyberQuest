d9f60c46d026643b5ec0647c07ee2caf5d2ae8d4
Robin Lagoda <robinou59880@gmail.com>
2025-09-08 10:50:53 +0200

    main.go

--- src/main.go
+++ src/main.go
@@ -1,1 +1,52 @@
-package src
+package main
+
+// importer les packages necessaires ici
+
+func initCharacter() {
+	// initialiser les personnages ici
+}
+
+func main() {
+	initCharacter()
+}
+
+func characterCreation() {
+	// creer un personnage ici
+	// ne pas oublier les classes
+	// utiliser des switch case
+}
+
+func displayInfo() {
+	// afficher les infos des personnages ici
+}
+
+func accesInventory() {
+	// acceder a l'inventaire ici
+}
+
+func addInventory() {
+	// ajouter un objet a l'inventaire ici
+	// gerer la capacite maximale
+}
+
+func removeInventory() {
+	// retirer un objet de l'inventaire ici
+}
+
+func upgradeIventorySlot() {
+	// augmenter la capacite maximale de l'inventaire ici
+}
+
+func takePot() {
+	// prendre une potion ici
+}
+
+// switch case menu
+
+func poisonPot() {
+	// appliquer l'effet de la potion empoisonnee ici
+}
+
+func isDead() {
+	// verifier si le personnage est mort ici
+}
