6a9782975deaee7e2b63ac34d2aa848021ad7bf5
leo-dpy <leo.dupuy@ynov.com>
2025-09-09 16:14:21 +0200

    takepot

--- src/RemoveInventory.go
+++ src/RemoveInventory.go
@@ -1,5 +1,31 @@
 package main
 
-func RemoveInventory() {
-	// retirer un objet de l'inventaire ici
+import (
+	"fmt"
+)
+
+// Supposons que l'inventaire est un slice de string dans le personnage
+func takePot() {
+	potionIndex := -1
+	for i, item := range player.Equipment.Loot {
+		if item == "Potion" {
+			potionIndex = i
+			break
+		}
+	}
+	if potionIndex == -1 {
+		fmt.Println("Aucune potion dans l'inventaire.")
+		return
+	}
+
+	// Supprimer la potion
+	player.Equipment.Loot = append(player.Equipment.Loot[:potionIndex], player.Equipment.Loot[potionIndex+1:]...)
+
+	// Soigner le joueur
+	player.HP += 50
+	if player.HP > player.MaxHP {
+		player.HP = player.MaxHP
+	}
+
+	fmt.Printf("Vous avez utilisé une potion ! PV : %d/%d\n", player.HP, player.MaxHP)
 }
--- src/TakePot.go
+++ src/TakePot.go
@@ -1,5 +1,40 @@
 package main
 
-func TakePot() {
-	// prendre une potion ici
+import (
+	"fmt"
+)
+
+// Exemple de structure Player
+type Player struct {
+	HP        int
+	MaxHP     int
+	Inventory []string
+}
+
+// Fonction pour utiliser une potion
+func TakePot(p *Player) {
+	potionIndex := -1
+	for i, item := range p.Inventory {
+		if item == "potion" {
+			potionIndex = i
+			break
+		}
+	}
+
+	if potionIndex == -1 {
+		fmt.Println("Vous n'avez pas de potion dans votre inventaire.")
+		return
+	}
+
+	// Consommer la potion
+	p.Inventory = append(p.Inventory[:potionIndex], p.Inventory[potionIndex+1:]...)
+
+	// Soigner le joueur
+	heal := 50
+	p.HP += heal
+	if p.HP > p.MaxHP {
+		p.HP = p.MaxHP
+	}
+
+	fmt.Printf("Vous avez utilisé une potion ! Points de vie : %d/%d\n", p.HP, p.MaxHP)
 }
