4d6bbde732d30fba23e89ff42b5b96c798eb9be0
leo-dpy <leo.dupuy@ynov.com>
2025-09-10 12:17:00 +0200

    combat1.0

--- src/initGoblin.go
+++ src/initGoblin.go
@@ -3,11 +3,11 @@ package main
 // Fonction pour initialiser un Gobelin d'entraînement
 func InitGoblin() Monster {
 	return Monster{
-		Name:   "Gobelin d'entrainement",
-		HP:     40,
-		MaxHP:  40,
-		Attack: 5,
-		Experience:     10,
-		BTC:    5,
+		Name:       "Gobelin d'entrainement",
+		HP:         40,
+		MaxHP:      40,
+		Attack:     5,
+		Experience: 10,
+		BTC:        5,
 	}
 }
