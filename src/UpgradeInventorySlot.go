124d837b4e10fd5483bcba9901079eb5ebdd09e5
leo-dpy <leo.dupuy@ynov.com>
2025-09-10 14:13:43 +0200

    poison

--- src/UpgradeInventorySlot.go
+++ src/UpgradeInventorySlot.go
@@ -1,5 +1,22 @@
 package main
 
-func UpgradeInventorySlot() {
-	// augmenter la capacite maximale de l'inventaire ici
+var MaxInventoryCapacity int = 20 // capacité initiale
+var InventoryUpgradeUsed int = 0
+
+func UpgradeInventorySlot() bool {
+	if InventoryUpgradeUsed < 3 {
+		MaxInventoryCapacity += 10
+		InventoryUpgradeUsed++
+		return true
+	}
+	return false
+}
+
+// Exemple d'ajout dans le marchand
+func BuyInventoryUpgrade(playerGold *int) bool {
+	if *playerGold >= 30 && InventoryUpgradeUsed < 3 {
+		*playerGold -= 30
+		return UpgradeInventorySlot()
+	}
+	return false
 }
