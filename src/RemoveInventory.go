613aa2b5c1b8c9e3107677def60869193aa2b9f1
Robin Lagoda <robinou59880@gmail.com>
2025-09-18 16:00:38 +0200

    FR

--- src/RemoveInventory.go
+++ src/RemoveInventory.go
@@ -5,7 +5,7 @@ func RemoveInventory(c *Character, item int) {
 	for i, v := range c.Inventory {
 		if v == c.Inventory[item-1] {
 			// On enlève l'élément i du slice
-			c.Inventory = append(c.Inventory[:i], c.Inventory[i-1:]...)
+			c.Inventory = append(c.Inventory[:i-1], c.Inventory[i-1:]...)
 			return
 		}
 	}
